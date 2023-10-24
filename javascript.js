ff90a7b00bee19be285bdfa019a9895818c888a4
livknapen <59344183+livknapen@users.noreply.github.com>
2023-10-24 22:30:37 +0200

    luffy added on design/im/research

--- javascript.js
+++ javascript.js
@@ -8,3 +8,13 @@ function closeNav() {
     document.getElementById("burger").style.display = "revert";
 }
 
+window.addEventListener(
+    "scroll",
+    () => {
+      document.body.style.setProperty(
+        "--scroll",
+        window.pageYOffset / (document.body.offsetHeight - window.innerHeight)
+      );
+    },
+    false
+  );
